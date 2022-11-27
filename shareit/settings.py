69231153ee8cef184130348a9ce531bcf6a4685d
Amit Bisht <a.bisht.289@gmail.com>
2022-11-27 21:41:47 +0530

    Added new files

--- shareit/settings.py
+++ shareit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8y&66*@v2o-3ye(emx5jq*yb-vtqs%r17x4dsrvs%9#$x)2!k6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
